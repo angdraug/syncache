e899ab1f93c10e0f6a682dc1f6f19fdad638e882
Dmitry Borodaenko <angdraug@debian.org>
2011-06-04 21:07:47 +0300

    first commit to github

---
+++ lib/syncache_sync_patch.rb
@@ -0,0 +1,36 @@
+# Monkey patch for standard sync.rb (see bug #11680 on RubyForge).
+
+if RUBY_VERSION < "1.8.7" or (RUBY_VERSION == "1.8.7" and RUBY_PATCHLEVEL < 173)
+
+module Sync_m
+  class Err < StandardError
+    def Err.Fail(*opt)
+      Thread.critical = false
+      fail self, sprintf(self::Message, *opt)
+    end
+  end
+
+  def sync_try_lock(mode = EX)
+    return unlock if mode == UN
+
+    Thread.critical = true
+    ret = sync_try_lock_sub(mode)
+    Thread.critical = false
+    ret
+  end
+end
+
+elsif RUBY_VERSION == "1.9.0"
+
+module Sync_m
+  def sync_try_lock(mode = EX)
+    return unlock if mode == UN
+    ret = nil
+    @sync_mutex.synchronize do
+      ret = sync_try_lock_sub(mode)
+    end
+    ret
+  end
+end
+
+end
---
+++ test/ts_syncache.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/env ruby
+#
+# SynCache tests
+#
+#   Copyright (c) 2002-2009  Dmitry Borodaenko <angdraug@debian.org>
+#
+#   This program is free software.
+#   You can distribute/modify this program under the terms of
+#   the GNU General Public License version 3 or later.
+#
+# vim: et sw=2 sts=2 ts=8 tw=0
+
+require 'test/unit'
+require 'syncache'
+
+include SynCache
+
+class TC_Cache < Test::Unit::TestCase
+
+  def test_initialize
+    cache = Cache.new(3, 5)
+  end
+
+  def test_flush
+    cache = Cache.new(3, 5)
+    cache['t'] = 'test'
+    cache.flush
+    assert_equal nil, cache['t']
+  end
+
+  def test_add_fetch
+    cache = Cache.new(3, 5)
+    cache['t'] = 'test'
+    assert_equal 'test', cache['t']
+  end
+
+  def test_fetch_or_add
+    cache = Cache.new(3, 5)
+    assert_equal nil, cache['t']
+    cache.fetch_or_add('t') { 'test' }
+    assert_equal 'test', cache['t']
+  end
+
+  def test_truncate
+    cache = Cache.new(3, 5)
+    1.upto(5) {|i| cache[i] = i }
+    1.upto(5) do |i|
+      assert_equal i, cache[i]
+    end
+    6.upto(10) {|i| cache[i] = i }
+    1.upto(5) do |i|
+      assert_equal nil, cache[i]
+    end
+  end
+
+  def test_timeout
+    cache = Cache.new(0.01, 5)
+    1.upto(5) {|i| cache[i] = i }
+    1.upto(5) do |i|
+      assert_equal i, cache[i]
+    end
+    sleep(0.02)
+    1.upto(5) do |i|
+      assert_equal nil, cache[i]
+    end
+  end
+end
